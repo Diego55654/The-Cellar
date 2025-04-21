7dfbfff065a07b05ec3b2806ae7bb71b48c21af3
Daniel Paulino <160649011+thedanielpaulino@users.noreply.github.com>
2025-04-21 20:59:53 -0300

    Fix SenhaUtils.java
    Erro de importação corrigido

--- app/src/main/java/com/example/game/utils/SenhaUtils.java
+++ app/src/main/java/com/example/game/utils/SenhaUtils.java
@@ -5,7 +5,7 @@ import android.os.Build;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 import java.security.SecureRandom;
-import java.util.Base64;
+import android.util.Base64;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 
