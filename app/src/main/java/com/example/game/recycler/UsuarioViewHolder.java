d290389fffd1ac64ab6ad269245ab04f246db78d
Daniel Paulino <160649011+thedanielpaulino@users.noreply.github.com>
2025-04-21 20:14:02 -0300

    Update UsuarioViewHolder.java

--- app/src/main/java/com/example/game/recycler/UsuarioViewHolder.java
+++ app/src/main/java/com/example/game/recycler/UsuarioViewHolder.java
@@ -1,17 +1,24 @@
 package com.example.game.recycler;
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.game.R;
 
 public class UsuarioViewHolder extends RecyclerView.ViewHolder {
     public TextView txtNome;
     public TextView txtEmail;
+    public Button btnEditar;
+    public Button btnExcluir;
 
     public UsuarioViewHolder(View itemView) {
         super(itemView);
         txtNome = itemView.findViewById(R.id.txtNome);
         txtEmail = itemView.findViewById(R.id.txtEmail);
+        btnEditar = itemView.findViewById(R.id.btnEditar);
+        btnExcluir = itemView.findViewById(R.id.btnExcluir);
     }
 }
